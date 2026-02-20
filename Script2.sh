774ffb2bd664a95751c54f52cc745a7ff9b1a8f3
bansoderutuja <bansoderutuja1234@gmail.com>
2026-02-20 17:54:49 +0530

    learning branching

--- Script2.sh
+++ Script2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #This script is to read input from the user/console
-#This is variable scripting example
+#This is variable scripting 
 echo "Value of a"
 read a
 echo "Value of b"
 read b
 echo "Hello value of a is $a and value of b is $b"
+read c
+echo "Hello value of c is $c"
